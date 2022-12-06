f2e5183bb57753a3baf5af3f2cfaff6d2113fe39
vinayaka b shetty <vinayakasjce@gmail.com>
2022-12-06 23:07:02 +0530

    updated driver path

--- Maven_Selenium_Cucumber_Project/src/test/java/testcases/NavigateToGoogle.java
+++ Maven_Selenium_Cucumber_Project/src/test/java/testcases/NavigateToGoogle.java
@@ -7,8 +7,7 @@ import org.testng.annotations.Test;
 public class NavigateToGoogle {
 	@Test
 	public void navigateToHomePage() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Admin\\Desktop\\Eclipse workspace\\Maven_Selenium_Cucumber_Project\\src\\test\\resources\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
