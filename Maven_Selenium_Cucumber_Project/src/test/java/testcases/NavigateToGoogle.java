6aaf2356cf9081b06c6e4cf07b38f6ca33f4f983
vinayaka b shetty <vinayakasjce@gmail.com>
2022-12-07 14:15:10 +0530

    3rd trial

--- Maven_Selenium_Cucumber_Project/src/test/java/testcases/NavigateToGoogle.java
+++ Maven_Selenium_Cucumber_Project/src/test/java/testcases/NavigateToGoogle.java
@@ -11,7 +11,7 @@ public class NavigateToGoogle {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
-		System.out.println("Finally , Build triggered from Git Push webhook 2nd trial");
+		System.out.println("Finally , Build triggered from Git Push webhook 3rd trial");
 		driver.quit();
 	}
 }
